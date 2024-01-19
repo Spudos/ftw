8720ca74dfcc1e12b3305d4e675bee366980a024
Andy Pierce <spudos16@gmail.com>
2024-01-19 17:28:16 +0000

    stadium test working

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,19 +1,41 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe Match, type: :model do
   describe 'stadium_size calculation test' do
-    it "should return the number total number of seats" do
-      totals_with_blend = [ team: '001' ]
-      club = build(:club,
+    it "should return the total number of seats" do
+      totals_with_blend = [{ team: '001' }]
+      match = Match.new
+
+      create(:club,
         stand_n_capacity: 1000,
         stand_s_capacity: 2000,
         stand_e_capacity: 3000,
         stand_w_capacity: 4000
       )
 
-      stadium_size_answer = totals_with_blend.send(:stadium_size, totals_with_blend)
+      stadium_size_answer = match.send(:stadium_size, totals_with_blend)
 
       expect(stadium_size_answer).to be == 10000
     end
   end
+
+  describe 'sqaud with tactics calculation' do
+    it "should return correct adjusted values" do
+
+      squad_with_performance = {
+        match_performance: 50,
+        player_position_detail: 'c',
+        player_position: 'dfc',
+        tactic: 1
+      }
+
+      match = Match.new
+
+      adjusted_player = match.send(:player_performance_by_tactic, squad_with_performance)
+
+      expect(adjusted_player.match_performance).to be == 45
+    end
+  end
 end
+
