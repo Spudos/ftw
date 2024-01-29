fc4f8a35c11956087e0a32f3feb7bdd28760441e
Andy Pierce <spudos16@gmail.com>
2024-01-29 14:05:55 +0000

    refactored match tests

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -20,201 +20,47 @@ RSpec.describe Match, type: :model do
     end
   end
 
-  describe 'sqaud with tactics calculation' do
-    it 'should return correct adjusted values for tactic 1' do
-      squad_with_performance = [
+  describe 'squad with tactics calculation' do
+    before do
+      @squad_with_performance = [
         {
           match_performance: 50,
           player_position_detail: 'c',
           player_position: 'dfc',
-          tactic: 1
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'mid',
-          tactic: 1
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'att',
-          tactic: 1
-        }
-      ]
-
-      adjusted_player = Match::TacticAdjustment.new(squad_with_performance).player_performance_by_tactic
-      expect(adjusted_player[0][:match_performance]).to be == 45
-      expect(adjusted_player[1][:match_performance]).to be == 65
-      expect(adjusted_player[2][:match_performance]).to be == 45
-    end
-
-    it 'should return correct adjusted values for tactic 2' do
-      squad_with_performance = [
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'dfc',
-          tactic: 2
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'mid',
-          tactic: 2
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'att',
-          tactic: 2
-        }
-      ]
-
-      adjusted_player = Match::TacticAdjustment.new(squad_with_performance).player_performance_by_tactic
-      expect(adjusted_player[0][:match_performance]).to be == 65
-      expect(adjusted_player[1][:match_performance]).to be == 40
-      expect(adjusted_player[2][:match_performance]).to be == 40
-    end
-
-    it 'should return correct adjusted values for tactic 3' do
-      squad_with_performance = [
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'dfc',
-          tactic: 3
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'mid',
-          tactic: 3
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'att',
-          tactic: 3
-        }
-      ]
-
-      adjusted_player = Match::TacticAdjustment.new(squad_with_performance).player_performance_by_tactic
-      expect(adjusted_player[0][:match_performance]).to be == 40
-      expect(adjusted_player[1][:match_performance]).to be == 55
-      expect(adjusted_player[2][:match_performance]).to be == 65
-    end
-
-    it 'should return correct adjusted values for tactic 4' do
-      squad_with_performance = [
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'dfc',
-          tactic: 4
+          tactic: nil
         },
         {
           match_performance: 50,
           player_position_detail: 'l',
           player_position: 'mid',
-          tactic: 4
+          tactic: nil
         },
         {
           match_performance: 50,
           player_position_detail: 'r',
           player_position: 'att',
-          tactic: 4
-        }
-      ]
-
-      adjusted_player = Match::TacticAdjustment.new(squad_with_performance).player_performance_by_tactic
-      expect(adjusted_player[0][:match_performance]).to be == 40
-      expect(adjusted_player[1][:match_performance]).to be == 60
-      expect(adjusted_player[2][:match_performance]).to be == 60
-    end
-
-    it 'should return correct adjusted values for tactic 5' do
-      squad_with_performance = [
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'dfc',
-          tactic: 5
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'l',
-          player_position: 'mid',
-          tactic: 5
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'r',
-          player_position: 'att',
-          tactic: 5
-        }
-      ]
-
-      adjusted_player = Match::TacticAdjustment.new(squad_with_performance).player_performance_by_tactic
-      expect(adjusted_player[0][:match_performance]).to be == 60
-      expect(adjusted_player[1][:match_performance]).to be == 40
-      expect(adjusted_player[2][:match_performance]).to be == 40
-    end
-
-    it 'should return correct adjusted values for tactic 6' do
-      squad_with_performance = [
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'dfc',
-          tactic: 6
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'mid',
-          tactic: 6
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'att',
-          tactic: 6
-        }
-      ]
-
-      adjusted_player = Match::TacticAdjustment.new(squad_with_performance).player_performance_by_tactic
-      expect(adjusted_player[0][:match_performance]).to be == 55
-      expect(adjusted_player[1][:match_performance]).to be == 45
-      expect(adjusted_player[2][:match_performance]).to be == 55
-    end
-
-    it 'should return original values for nil tactic' do
-      squad_with_performance = [
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'dfc',
-          tactic: nil
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'mid',
-          tactic: nil
-        },
-        {
-          match_performance: 50,
-          player_position_detail: 'c',
-          player_position: 'att',
           tactic: nil
         }
       ]
+    end
 
-      adjusted_player = Match::TacticAdjustment.new(squad_with_performance).player_performance_by_tactic
-      expect(adjusted_player[0][:match_performance]).to be == 50
-      expect(adjusted_player[1][:match_performance]).to be == 50
-      expect(adjusted_player[2][:match_performance]).to be == 50
+    [
+      { tactic: 1, expected_results: [45, 65, 45] },
+      { tactic: 2, expected_results: [65, 40, 40] },
+      { tactic: 3, expected_results: [40, 55, 65] },
+      { tactic: 4, expected_results: [40, 60, 60] },
+      { tactic: 5, expected_results: [60, 40, 40] },
+      { tactic: 6, expected_results: [55, 45, 55] }
+    ].each do |test_case|
+      it "should return correct adjusted values for tactic #{test_case[:tactic]}" do
+        @squad_with_performance.each { |player| player[:tactic] = test_case[:tactic] }
+
+        adjusted_player = Match::TacticAdjustment.new(@squad_with_performance).player_performance_by_tactic
+
+        test_case[:expected_results].each_with_index do |expected_result, index|
+          expect(adjusted_player[index][:match_performance]).to be == expected_result
+        end
+      end
     end
   end
 end
