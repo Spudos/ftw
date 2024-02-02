6224b5932e3595f54437a5f99ec4b380e03fcce6
Andy Pierce <spudos16@gmail.com>
2024-02-02 13:14:23 +0000

    possesion test with guard added to method

--- app/models/match/possession.rb
+++ app/models/match/possession.rb
@@ -6,8 +6,16 @@ class Match::Possession
   end
 
   def call
-    home_possession = (match_summary[:chance_count_home] / (match_summary[:chance_count_home] + match_summary[:chance_count_away]).to_f * 80).to_i
-    away_possession = 100 - home_possession
+    if (match_summary[:chance_count_home] + match_summary[:chance_count_away]) == 0
+      away_possession = 48
+    else
+      away_possession = (match_summary[:chance_count_away] / (match_summary[:chance_count_home] + match_summary[:chance_count_away]).to_f * 80).to_i
+      if away_possession <= 20
+        away_possession = rand(20..30)
+      end
+    end
+
+    home_possession = 100 - away_possession
 
     { home_possession:, away_possession:}
   end
