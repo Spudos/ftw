a791f9be39a29d6cdb59f62ba383598b1795b71e
Andy Pierce <spudos16@gmail.com>
2024-02-04 16:01:07 +0000

    add error handing to selections controller

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -1,50 +1,39 @@
 class SelectionsController < ApplicationController
-  before_action :set_selection, only: %i[ show edit update destroy ]
-
-  # GET /selections or /selections.json
+  before_action :set_selection, only: %i[show edit update destroy]
   def index
     @selections = Selection.all
   end
 
-  # GET /selections/1 or /selections/1.json
-  def show
-  end
+  def show; end
 
-  # GET /selections/new
   def new
     @selection = Selection.new
   end
 
-  # GET /selections/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /selections or /selections.json
   def create
     club = Club.find_by(abbreviation: params[:club])
-    player_ids = params[:player_ids].map(&:to_i)
+    if params[:player_ids].nil? || params[:player_ids].count != 11
+      flash[:alert] = 'You must select 11 players.'
+      redirect_to club_path(params[:club])
+    else
+      player_ids = params[:player_ids].map(&:to_i)
 
-    if player_ids.length == 11
-      # Delete previous entries for the selected club
       Selection.where(club: club.abbreviation).delete_all
 
-      # Save the new selection
       player_ids.each do |player_id|
-
-        Selection.create(club: club.abbreviation, player_id: player_id)
+        Selection.create(club: club.abbreviation, player_id:)
       end
-
-      redirect_to clubs_path, notice: "Selection saved successfully."
-    else
-      render club
+      flash[:notice] = 'Selection saved successfully.'
+      redirect_to club_path(params[:club])
     end
   end
 
-  # PATCH/PUT /selections/1 or /selections/1.json
   def update
     respond_to do |format|
       if @selection.update(selection_params)
-        format.html { redirect_to selection_url(@selection), notice: "Selection was successfully updated." }
+        format.html { redirect_to selection_url(@selection), notice: 'Selection was successfully updated.' }
         format.json { render :show, status: :ok, location: @selection }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,24 +42,21 @@ class SelectionsController < ApplicationController
     end
   end
 
-  # DELETE /selections/1 or /selections/1.json
   def destroy
     @selection.destroy
 
     respond_to do |format|
-      format.html { redirect_to selections_url, notice: "Selection was successfully destroyed." }
+      format.html { redirect_to selections_url, notice: 'Selection was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_selection
     @selection = Selection.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def selection_params
     params.require(:selection).permit(:club, :player_id)
   end
