13711d902c24b885c7999e2ba8948239db7a6273
Andy Pierce <spudos16@gmail.com>
2024-02-27 16:53:37 +0000

    minor spec fix

--- spec/models/Turn/articles_updates_spec.rb
+++ spec/models/Turn/articles_updates_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Turn::ArticleUpdates, type: :model do
       create(:club, id: 1, name: 'clubby')
       create(:club, id: 2, name: 'cluubby')
       create(:club, id: 3, name: 'cluuubby')
+      create(:club, id: 4, name: 'cluuubby')
       create(:player, club_id: 1, average_performance: 60, name: 'John', total_goals: 100, total_assists: 10)
       create(:player, club_id: 1, average_performance: 50, name: 'Johnny', total_goals: 90, total_assists: 20)
       create(:player, club_id: 2, average_performance: 40, name: 'Jon', total_goals: 80, total_assists: 30)
@@ -16,7 +17,7 @@ RSpec.describe Turn::ArticleUpdates, type: :model do
       create(:player, club_id: 3, average_performance: 10, name: 'Johhny', total_goals: 50, total_assists: 60)
 
       Turn::ArticleUpdates.new(week).call
-binding.pry
+
       expect(Article.first.article_type).to eq('Player')
     end
   end
