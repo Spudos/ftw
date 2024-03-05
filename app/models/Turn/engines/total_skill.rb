5b5e1a551b0eb304bd5208cc829e40658fdbf4fe
Jean Petric <petricomp@gmail.com>
2024-03-05 19:43:39 +0100

    Optimise counting

--- app/models/Turn/engines/total_skill.rb
+++ app/models/Turn/engines/total_skill.rb
@@ -13,8 +13,14 @@ class Turn::Engines::TotalSkill
   private
 
   def player_total_skill
+    # File.open('timings.txt', 'w') do |file|
+    #   file.write("Started: #{Time.current}")
+    # end
     players.each do |player|
       player.total_skill = player.total_skill
     end
+    # File.open('timings2.txt', 'w') do |file|
+    #   file.write("Ended: #{Time.current}")
+    # end
   end
 end
