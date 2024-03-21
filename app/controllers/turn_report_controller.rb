decd1477485764129088f05837fdbe4bfa0ab9e6
Andy Pierce <spudos16@gmail.com>
2024-03-21 10:38:02 +0000

    remove turn view unless logged in

--- app/controllers/turn_report_controller.rb
+++ app/controllers/turn_report_controller.rb
@@ -9,9 +9,6 @@ class TurnReportController < ApplicationController
     @gm_messages = Message.where(var2: 'gm', week: params[:week_number])
     @public_messages = Message.where(var2: 'public', week: params[:week_number])
     @game_messages = Message.where(var2: 'game', week: params[:week_number])
-
-    return unless current_user && current_user.club != 0
-
     @club = Club.find_by(id: current_user.club)
     @turn_present = Turnsheet.find_by(club_id: current_user.club, week: params[:week_number])
     @club_matches = Match.where(week_number: params[:week_number],
