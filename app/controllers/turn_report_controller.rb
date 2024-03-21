59a65724dc168e9c7172019e476522fc07c81757
Andy Pierce <spudos16@gmail.com>
2024-03-21 15:08:31 +0000

    remove club info from turnsheet

--- app/controllers/turn_report_controller.rb
+++ app/controllers/turn_report_controller.rb
@@ -1,19 +1,8 @@
 class TurnReportController < ApplicationController
   def index
-    if Message.any?
-      @weeks = Message.all.map(&:week).uniq.sort
-    else
-      @weeks = []
-    end
-
+    @weeks = Message.all.map(&:week).uniq.sort
     @gm_messages = Message.where(var2: 'gm', week: params[:week_number])
     @public_messages = Message.where(var2: 'public', week: params[:week_number])
     @game_messages = Message.where(var2: 'game', week: params[:week_number])
-    @club = Club.find_by(id: current_user.club)
-    @turn_present = Turnsheet.find_by(club_id: current_user.club, week: params[:week_number])
-    @club_matches = Match.where(week_number: params[:week_number],
-                                home_team: current_user.club)
-                         .or(Match.where(week_number: params[:week_number],
-                                         away_team: current_user.club))
   end
 end
