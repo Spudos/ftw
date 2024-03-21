99d1aaa187ed4c824ae9caf504a9c01686aa4d47
Andy Pierce <spudos16@gmail.com>
2024-03-21 10:30:29 +0000

    fix bug in message variable

--- app/controllers/turn_report_controller.rb
+++ app/controllers/turn_report_controller.rb
@@ -1,17 +1,22 @@
 class TurnReportController < ApplicationController
   def index
-    @weeks = Message.all.map(&:week).uniq.sort
+    if Message.any?
+      @weeks = Message.all.map(&:week).uniq.sort
+    else
+      @weeks = []
+    end
+
     @gm_messages = Message.where(var2: 'gm', week: params[:week_number])
     @public_messages = Message.where(var2: 'public', week: params[:week_number])
     @game_messages = Message.where(var2: 'game', week: params[:week_number])
 
-    if current_user && current_user.club != 0
+    return unless current_user && current_user.club != 0
+
     @club = Club.find_by(id: current_user.club)
     @turn_present = Turnsheet.find_by(club_id: current_user.club, week: params[:week_number])
     @club_matches = Match.where(week_number: params[:week_number],
                                 home_team: current_user.club)
                          .or(Match.where(week_number: params[:week_number],
                                          away_team: current_user.club))
-    end
   end
 end
