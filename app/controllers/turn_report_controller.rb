87fc6f30ce2a6c10078513082be352c2418d7fb3
Andy Pierce <spudos16@gmail.com>
2024-03-21 16:25:12 +0000

    add outstanding feedback warning

--- app/controllers/turn_report_controller.rb
+++ app/controllers/turn_report_controller.rb
@@ -1,6 +1,6 @@
 class TurnReportController < ApplicationController
   def index
-    @weeks = 1 # Message.all.map(&:week).uniq.sort
+    @weeks = Message.all.map(&:week).uniq.sort
     @gm_messages = Message.where(var2: 'gm', week: params[:week_number])
     @public_messages = Message.where(var2: 'public', week: params[:week_number])
     @game_messages = Message.where(var2: 'game', week: params[:week_number])
