35dbe648922abb996b65ec9c4620c7213ba02193
Jean Petric <petricomp@gmail.com>
2024-04-30 19:01:24 +0200

    Optimise iterations

--- app/models/Turn/engines/fitness.rb
+++ app/models/Turn/engines/fitness.rb
@@ -7,59 +7,57 @@ class Turn::Engines::Fitness
   end
 
   def process
-    availability
-    fitness_increase
-    random_injury
-    injury
+    sped_up_version
   end
 
   private
 
-  def availability
+  def sped_up_version
     players.each do |player|
-      if player.available.positive?
-        player.available -= 1
-        if player.available.zero?
-          Message.create(week:,
-                         action_id: "#{week}#{player.club_id}fitness",
-                         club_id: player.club_id,
-                         var1: "#{player.name} is now available for selection after injury")
-        end
-      end
+      availability(player)
+      fitness_increase(player)
+      random_injury(player)
+      injury(player)
     end
   end
 
-  def fitness_increase
-    players.each do |player|
-      player.fitness += rand(0..5)
-      player.fitness = 100 if player.fitness > 100
-    end
+  def availability(player)
+    return if player.available.negative?
+
+    player.available -= 1
+
+    return if player.available.positive?
+
+    Message.create(week:,
+                   action_id: "#{week}#{player.club_id}fitness",
+                   club_id: player.club_id,
+                   var1: "#{player.name} is now available for selection after injury")
   end
 
-  def random_injury
-    players.each do |player|
-      if rand(1..100) <= 2
-        player.fitness -= rand(20..40)
-        Message.create(week:,
-                       action_id: "#{week}#{player.club_id}fitness",
-                       club_id: player.club_id,
-                       var1: "#{player.name} took a bad knock in training this week")
-      end
-    end
+  def fitness_increase(player)
+    player.fitness += rand(0..5)
+    player.fitness = 100 if player.fitness > 100
   end
 
-  def injury
-    players.each do |player|
-      if player.fitness < 60
-        player.available = rand(1..9)
-        Message.create(week:,
-                       action_id: "#{week}#{player.club_id}fitness",
-                       club_id: player.club_id,
-                       var1: "#{player.name} has been injured and will be out for #{player.available} weeks")
-        if Selection.exists?(player_id: player.id)
-          Selection.find_by(player_id: player.id).destroy
-        end
-      end
-    end
+  def random_injury(player)
+    return unless rand(1..100) <= 2
+
+    player.fitness -= rand(20..40)
+    Message.create(week:,
+                   action_id: "#{week}#{player.club_id}fitness",
+                   club_id: player.club_id,
+                   var1: "#{player.name} took a bad knock in training this week")
+  end
+
+  def injury(player)
+    return unless player.fitness < 60
+
+    player.available = rand(1..9)
+    Message.create(week:,
+                   action_id: "#{week}#{player.club_id}fitness",
+                   club_id: player.club_id,
+                   var1: "#{player.name} has been injured and will be out for #{player.available} weeks")
+
+    Selection.find_by(player_id: player.id).destroy if Selection.exists?(player_id: player.id)
   end
 end
