068eb2224f621db0797355f6f33e013ef2162ca8
Andy Pierce <spudos16@gmail.com>
2024-08-14 01:01:55 +0100

    match commentary

--- app/models/match/match_end/match_end_commentary.rb
+++ app/models/match/match_end/match_end_commentary.rb
@@ -1,77 +1,122 @@
-class Match::SaveMatchCommentary
-  attr_reader :home_list, :away_list, :minute_by_minute
+class Match::MatchEnd::MatchEndCommentary
+  attr_reader :summary, :selection_complete, :fixture_attendance
 
-  def initialize(home_list, away_list, minute_by_minute)
-    @home_list = home_list
-    @away_list = away_list
-    @minute_by_minute = minute_by_minute
+  def initialize(summary, selection_complete, fixture_attendance)
+    @summary = summary
+    @selection_complete = selection_complete
+    @fixture_attendance = fixture_attendance
   end
 
   def call
-    if @home_list.nil? || @away_list.nil? || @minute_by_minute.nil?
+    if @summary.nil? || @selection_complete.nil? || @fixture_attendance.nil?
       raise StandardError, "There was an error in the #{self.class.name} class"
     end
 
-    home_team = Club.find_by(id: minute_by_minute.first[:club_home])&.name
-    away_team = Club.find_by(id: minute_by_minute.first[:club_away])&.name
+    commentary_upload = []
 
+    fixture_iteration(commentary_upload)
+
+    commentary_attributes = commentary_upload.to_a.map(&:as_json)
+    Commentary.upsert_all(commentary_attributes)
+  end
+
+  def fixture_iteration(commentary_upload)
+    sorted = summary.group_by { |element| element[1][0][:club_id] }
+
+    fixture_attendance.each do |match|
+      match_detail = sorted.find { |hash| hash[0] == match[:club_home] }
+
+      home_players = selection_complete.select { |player| player[:club_id] == match[:club_home] }
+      away_players = selection_complete.select { |player| player[:club_id] == match[:club_away] }
+
+      generate_commentary_data(match, match_detail, home_players, away_players, commentary_upload)
+    end
+  end
+
+  def generate_commentary_data(match, match_detail, home_players, away_players, commentary_upload)
     home_score = 0
     away_score = 0
 
-    minute_by_minute.each do |minute|
-      general_commentary = Template.random_match_general_commentary
-      chance_commentary = Template.random_match_chance_commentary
-      chance_tar_commentary = Template.random_match_chance_tar_commentary
-      goal_commentary = Template.random_match_goal_commentary  
+    match_detail[1].each do |record|
+      home_filtered_list = home_players.reject { |player| player[:position] == 'gkp' }
+      away_filtered_list = away_players.reject { |player| player[:position] == 'gkp' }
 
-      home_filtered_list = home_list.select { |player| player[:player_position] != "gkp" }
-      home_name = Player.find_by(id: home_filtered_list.sample[:player_id])&.name
+      commentary_data = { match_id: match[:id],
+                          minute: record[0],
+                          home_team: match[:club_home],
+                          away_team: match[:club_away],
+                          chance_outcome: record[4][:chance_outcome],
+                          chance_target: record[5][:chance_on_target],
+                          goal_scored: record[6][:goal_scored],
+                          scorer: record[7][:scorer],
+                          assister: record[7][:assist],
+                          home_name: home_filtered_list.sample[:name],
+                          away_name: away_filtered_list.sample[:name] }
 
-      away_filtered_list = away_list.select { |player| player[:player_position] != "gkp" }
-      away_name = Player.find_by(id: away_filtered_list.sample[:player_id])&.name
-
-      time = minute[:minute]
-      match_id = minute[:id]
-
-      if minute[:goal_scored] == 'home'
-        scorer = Player.find_by(id: minute[:scorer])&.name
-        assister = Player.find_by(id: minute[:assist])&.name
-        home_score += 1
-        event = 'Home Goal'
-        commentary = goal_commentary.gsub('{team}', home_team).gsub('{assister}', assister).gsub('{scorer}', scorer)
-      elsif minute[:goal_scored] == 'away'
-        scorer = Player.find_by(id: minute[:scorer])&.name
-        assister = Player.find_by(id: minute[:assist])&.name
-        away_score += 1
-        event = 'Away Goal'
-        commentary = goal_commentary.gsub('{team}', away_team).gsub('{assister}', assister).gsub('{scorer}', scorer)
-      elsif minute[:chance_on_target] == 'home'
-        event = 'Good chance'
-        commentary = chance_tar_commentary.gsub('{team}', home_team).gsub('{player}', home_name)
-      elsif minute[:chance_on_target] == 'away'
-        event = 'Good chance'
-        commentary = chance_tar_commentary.gsub('{team}', away_team).gsub('{player}', away_name)
-      elsif minute[:chance_outcome] == 'home'
-        event = 'Chance'
-        commentary = chance_commentary.gsub('{team}', home_team).gsub('{player}', home_name)
-      elsif minute[:chance_outcome] == 'away'
-        event = 'Chance'
-        commentary = chance_commentary.gsub('{team}', away_team).gsub('{player}', away_name)
-      else
-        event = ''
-        team_names = [away_team, home_team]
-        selected_team = team_names.sample
-        commentary = general_commentary.gsub('{team}', selected_team)
-      end
-
-      Commentary.create(
-        match_id: match_id,
-        minute: time,
-        commentary: commentary,
-        event: event,
-        home_score: home_score,
-        away_score: away_score
-      )
+      generate_commentary(commentary_data, commentary_upload, home_score, away_score)
     end
+  end
+
+  private
+
+  def generate_commentary(commentary_data, commentary_upload, home_score, away_score)
+    general_commentary, chance_commentary, chance_tar_commentary, goal_commentary = commentary_templates
+
+    if commentary_data[:goal_scored] == 'home'
+      scorer = selection_complete.find { |element| element[:player_id] == commentary_data[:scorer] }[:name]
+      assister = selection_complete.find { |element| element[:player_id] == commentary_data[:assister] }[:name]
+      home_score += 1
+      event = 'Home Goal'
+      commentary = goal_commentary.gsub('{team}', commentary_data[:home_team])
+                                  .gsub('{assister}', assister)
+                                  .gsub('{scorer}', scorer)
+    elsif commentary_data[:goal_scored] == 'away'
+      scorer = selection_complete.find { |element| element[:player_id] == commentary_data[:scorer] }[:name]
+      assister = selection_complete.find { |element| element[:player_id] == commentary_data[:assister] }[:name]
+      away_score += 1
+      event = 'Away Goal'
+      commentary = goal_commentary.gsub('{team}', commentary_data[:away_team])
+                                  .gsub('{assister}', assister)
+                                  .gsub('{scorer}', scorer)
+    elsif commentary_data[:chance_on_target] == 'home'
+      event = 'Good chance'
+      commentary = chance_tar_commentary.gsub('{team}', commentary_data[:home_team])
+                                        .gsub('{player}', commentary_data[:home_name])
+    elsif commentary_data[:chance_on_target] == 'away'
+      event = 'Good chance'
+      commentary = chance_tar_commentary.gsub('{team}', commentary_data[:away_team])
+                                        .gsub('{player}', commentary_data[:away_name])
+    elsif commentary_data[:chance_outcome] == 'home'
+      event = 'Chance'
+      commentary = chance_commentary.gsub('{team}', commentary_data[:home_team])
+                                    .gsub('{player}', commentary_data[:home_name])
+    elsif commentary_data[:chance_outcome] == 'away'
+      event = 'Chance'
+      commentary = chance_commentary.gsub('{team}', commentary_data[:away_team])
+                                    .gsub('{player}', commentary_data[:away_name])
+    else
+      event = ''
+      team_names = [commentary_data[:away_team], commentary_data[:home_team]]
+      selected_team = team_names.sample
+      commentary = general_commentary.gsub('{team}', selected_team)
+    end
+
+    commentary = { match_id: commentary_data[:match_id],
+                   minute: commentary_data[:minute],
+                   commentary:,
+                   event:,
+                   home_score:,
+                   away_score: }
+
+    commentary_upload << commentary
+  end
+
+  def commentary_templates
+    general_commentary = Template.random_match_general_commentary
+    chance_commentary = Template.random_match_chance_commentary
+    chance_tar_commentary = Template.random_match_chance_tar_commentary
+    goal_commentary = Template.random_match_goal_commentary
+
+    return general_commentary, chance_commentary, chance_tar_commentary, goal_commentary
   end
 end
