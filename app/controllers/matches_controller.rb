8371f6e1032d84747465d7a02ae8db85a2a253c6
Andy Pierce <spudos16@gmail.com>
2024-08-29 08:32:17 +0100

    fix match view issue

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,7 +5,7 @@ class MatchesController < ApplicationController
   def index; end
 
   def show
-    @player_match_data = Performance.where(match_id: params[:id])
+    @player_match_data = player_position_sort
     @matches = Match.where(match_id: params[:id])
     @goal_information = Goal.where(match_id: params[:id])
   end
@@ -36,5 +36,24 @@ class MatchesController < ApplicationController
 
   def handle_error(exception)
     redirect_to request.referrer, alert: exception.message
+  end
+
+  def player_position_sort
+    players = Performance.where(match_id: params[:id]).order(Arel.sql(
+                                                 "CASE
+                                                        WHEN player_position = 'gkp' AND player_position_detail = 'p' THEN 1
+                                                        WHEN player_position = 'dfc' AND player_position_detail = 'l' THEN 2
+                                                        WHEN player_position = 'dfc' AND player_position_detail = 'r' THEN 3
+                                                        WHEN player_position = 'dfc' AND player_position_detail = 'c' THEN 4
+                                                        WHEN player_position = 'mid' AND player_position_detail = 'l' THEN 5
+                                                        WHEN player_position = 'mid' AND player_position_detail = 'r' THEN 6
+                                                        WHEN player_position = 'mid' AND player_position_detail = 'c' THEN 7
+                                                        WHEN player_position = 'att' AND player_position_detail = 'l' THEN 8
+                                                        WHEN player_position = 'att' AND player_position_detail = 'r' THEN 9
+                                                        ELSE 10
+                                                        END"
+                                               ))
+
+    return players
   end
 end
