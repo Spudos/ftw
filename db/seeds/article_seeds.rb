7450e59009fe188f0360098db08b9bdb37b81f3f
Andy Pierce <spudos16@gmail.com>
2024-11-28 12:03:05 +0000

    add article seeds and more sign up messages

---
+++ db/seeds/article_seeds.rb
@@ -0,0 +1,53 @@
+Article.create(
+  week: 0,
+  image: 'fans.jpg',
+  article_type: 'news',
+  headline: 'Time for a new season',
+  sub_headline: 'Waiting list open!',
+  article: 'We are now accepting sign ups for the new season.  All you have to do is create a user and you will go onto the manager waiting list.  Once we are ready to begin team selection and creation we will email you to tell you what to do next.',
+)
+
+Article.create(
+  week: 0,
+  image: 'fans1.jpg',
+  article_type: 'news',
+  headline: 'Sign Up so see club details',
+  sub_headline: 'Example club enabled on sign up',
+  article: 'When you sign up you will be able to see an example club.  This will give you an idea of what you can expect when you are selected as a manager.  You will be able to see the club details, the players, and other game information.',
+)
+
+Article.create(
+  week: 0,
+  image: 'goal.jpg',
+  article_type: 'news',
+  headline: 'Hidden Player Stats',
+  sub_headline: 'Great Feature!',
+  article: 'All players have many different hidden stats such as consistency, potential, loyalty, and more.  These stats are not visible to the manager but will affect the player performance.',
+)
+
+Article.create(
+  week: 0,
+  image: 'player.jpg',
+  article_type: 'news',
+  headline: 'Our most fun and powerful player stat',
+  sub_headline: 'Blend',
+  article: 'Blend is a hidden stat that affects how well a player will play with other players.  This is a very important stat to consider when selecting your team.  Does your player fit in with the rest of the team?',
+)
+
+Article.create(
+  week: 0,
+  image: 'training.jpg',
+  article_type: 'news',
+  headline: 'Player Potential',
+  sub_headline: 'The future is bright (maybe)',
+  article: 'All players have a potential stat for every skill that will determine how much they can improve.  Some players will improve a lot and some will not improve at all.',
+)
+
+Article.create(
+  week: 0,
+  image: 'transfers.jpg',
+  article_type: 'news',
+  headline: 'Transfers',
+  sub_headline: 'Spendy McSpend',
+  article: 'You will be able to buy and sell players in the transfer market.  You can also loan players to other clubs.  Be careful not to spend too much money or you will be in trouble with the board.  No transfer deadline, buy, sell, and loan players all season long to build your dream team.',
+)
